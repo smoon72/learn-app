95ed366c7cfd28c4b597a5e85bdcadd0091f79fe
smoon72 <sjm9219@gmail.com>
2025-06-15 00:04:42 -0400

    useless stuff

--- learn-app/src/app/sidebar/sidebar.component.ts
+++ learn-app/src/app/sidebar/sidebar.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  standalone: true
 })
 export class SidebarComponent {
 
